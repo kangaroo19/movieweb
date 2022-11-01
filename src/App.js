6bca9b3aea96ea607255ab5780d656366636cdc8
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-11-01 15:39:23 +0900

    123

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ import Detail from "./routes/Detail";
 import React from 'react';
 function App() {
   return (
-  // <Router>
-  //   <Switch>
-  //     <Route path='/movie/:id'>
-  //       <Detail/> 
-  //     </Route>
-  //     <Route path='/'>
-  //       <Home/> 
-  //     </Route>
-  //   </Switch>
-  // </Router> 
-  <h1>asdfasdfasdfasdfasdf</h1>
+  <Router basename={process.env.PUBLIC_URL}>
+    <Switch>
+      <Route path='/movie/:id'>
+        <Detail/> 
+      </Route>
+      <Route path='/'>
+        <Home/> 
+      </Route>
+    </Switch>
+  </Router> 
 )}
 
 export default App;
